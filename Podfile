614867911f9438020c7b29d1fa8c218b61b78c0f
jjz <jiangjiazhi@ujuhui.com>
2015-08-07 18:52:07 +0800

    init project

---
+++ Podfile
@@ -0,0 +1,2 @@
+platform :ios, '7.0'
+pod ‘pod-library’, :path => ‘../pod-library’
---
+++ pod-project/ViewController.h
@@ -0,0 +1,16 @@
+//
+//  ViewController.h
+//  testproject
+//
+//  Created by NewUser on 15/8/6.
+//  Copyright (c) 2015年 jjz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UIImageView *iv;
+
+
+@end
+
